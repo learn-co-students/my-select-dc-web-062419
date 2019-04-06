eb2137942bf9f7bc7459a12ad8169c8d0a4e5f0d
lesliesage <leslie.sage@gmail.com>
2019-04-06 11:59:53 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    mod = []
+    i = 0
+    while i < collection.length
+      if yield(collection[i]) == true
+        mod << collection[i]
+      else
+        mod
+      end
+      i += 1
+    end
+    mod
+  else
+    nil
+  end
 end
+
+# my test (must add "puts" above to see result in console)
+# my_select([1,-3,10,3,2,4]) {|i| i.even?}
