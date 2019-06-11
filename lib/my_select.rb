8cb69bece62d1194c85ce4040393d615b9a60a90
Michelle Lam <shelly2.lam@gmail.com>
2019-06-11 12:02:18 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
  # your code here!
+ emptyArray = []
+ i = 0
+ while i < collection.length do
+    if yield collection[i]
+        emptyArray.push(collection[i])
+    end
+    i += 1
 end
+emptyArray
+end
+
+nums = [1, 2, 3, 4, 5]
+my_select(nums) do |num|
+    num.even?
+    end
+   
+
